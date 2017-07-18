9f18d9534414c4c60ede6cff8de6a20212028e95
Eranus <ecthomps@umich.edu>
2017-07-17 22:55:48 -0400

    completed dogCatPerson

--- DogOrCatPerson/app/src/main/java/christhomps/example/christianthompson/dogorcatperson/MainActivity.java
+++ DogOrCatPerson/app/src/main/java/christhomps/example/christianthompson/dogorcatperson/MainActivity.java
@@ -1,9 +1,13 @@
 package christhomps.example.christianthompson.dogorcatperson;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
@@ -39,7 +43,88 @@ public class MainActivity extends AppCompatActivity {
         cutestCheckBoxCat = (CheckBox) findViewById(R.id.checkBoxCutestCat);
         cutestCheckBoxParrot = (CheckBox) findViewById(R.id.checkBoxCutestParrot);
 
-        showResultButton = (Button) findViewById(R.id.showResult);
+        //call methods
+        processCutest(cutestCheckBoxDog, cutestCheckBoxCat, cutestCheckBoxParrot);
+        processDrool(droolRadioGroup);
+        processCanines(canineRadioGroup);
+        showSeekBar(seekBar, seekBarTextView);
 
+        showResultButton = (Button) findViewById(R.id.showResult);
+        showResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("catCounter", catCounter);
+                intent.putExtra("dogCounter", dogCounter);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void processCutest(CheckBox checkBoxDog, CheckBox checkBoxCat,
+                              CheckBox checkBoxParrot){
+
+        if(checkBoxDog.isChecked() && !checkBoxCat.isChecked() &&
+                  !checkBoxParrot.isChecked()){
+            dogCounter+=5;
+        } else if(checkBoxCat.isChecked() && !checkBoxDog.isChecked() &&
+                  !checkBoxParrot.isChecked()){
+            catCounter+=5;
+        }
+    }
+
+    public void processDrool(final RadioGroup radioGroup){
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                droolRadioButton = (RadioButton) findViewById(radioId);
+
+                if(droolRadioButton.getText().equals("Yes")){
+                    dogCounter+= 5;
+                }
+            }
+        });
+    }
+
+    public void processCanines(final RadioGroup radioGroup){
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                canineRadioButton = (RadioButton) findViewById(radioId);
+
+                if(canineRadioButton.getText().equals("Yes")){
+                    dogCounter+=5;
+                }
+            }
+        });
+    }
+
+    public void showSeekBar(SeekBar seekBar, final TextView textView){
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText("Comfortableness: " + progress + "/" + seekBar.getMax());
+                catCounter+=progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
--- DogOrCatPerson/app/src/main/java/christhomps/example/christianthompson/dogorcatperson/ResultActivity.java
+++ DogOrCatPerson/app/src/main/java/christhomps/example/christianthompson/dogorcatperson/ResultActivity.java
@@ -2,12 +2,38 @@ package christhomps.example.christianthompson.dogorcatperson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView result;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        result = (TextView) findViewById(R.id.petLoverId);
+        imageView = (ImageView) findViewById(R.id.showPet);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null){
+
+            int catResults = extras.getInt("catCounter");
+            int dogResults = extras.getInt("dogCounter");
+
+            if(catResults > dogResults){
+                result.setText("Cat Lover!!!");
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.cat));
+            } else if(dogResults > catResults){
+                result.setText("Dog Lover!!!");
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.dog));
+            } else{
+                result.setText("NEITHER!!!");
+            }
+        }
     }
 }
