b98d3bcd9314c61a687d27f830c7e2eebf2394b5
Eranus <ecthomps@umich.edu>
2017-07-24 12:49:05 -0400

    finished mediaPlayer functionality

--- MusicDemo/app/src/main/java/christhomps/example/christianthompson/musicdemo/MainActivity.java
+++ MusicDemo/app/src/main/java/christhomps/example/christianthompson/musicdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
         mediaPlayer = new MediaPlayer();
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.game_field);
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                int duration = mp.getDuration() / 1000;
+
+                Toast.makeText(getApplicationContext(), "Duration: " + duration + "seconds",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
         playMusic = (Button) findViewById(R.id.playBtnId);
         playMusic.setOnClickListener(new View.OnClickListener() {
@@ -47,5 +58,16 @@ public class MainActivity extends AppCompatActivity {
             mediaPlayer.start();
             playMusic.setText("Pause");
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        if(mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
