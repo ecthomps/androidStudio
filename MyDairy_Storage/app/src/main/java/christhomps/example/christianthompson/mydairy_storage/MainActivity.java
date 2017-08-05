7f6d4291b327522c0eb215bb398926d39143b68f
Eranus <ecthomps@umich.edu>
2017-08-05 17:17:35 -0400

    finished saveButton functionality

--- MyDairy_Storage/app/src/main/java/christhomps/example/christianthompson/mydairy_storage/MainActivity.java
+++ MyDairy_Storage/app/src/main/java/christhomps/example/christianthompson/mydairy_storage/MainActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         userText = (EditText) findViewById(R.id.editText);
         saveBtn = (Button) findViewById(R.id.saveBtn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            //check to see if users have entered something
+                if(!userText.getText().toString().equals("")){
+                    //if no text then write to file
+                    String data = userText.getText().toString();
+                    writeToFile(data);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter text",
+                                       Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        //check to see if file existed and display existing content on it
+        if(readfromFile() != null){
+            userText.setText(readfromFile());
+        }
     }
 
     public void writeToFile(String myData){
@@ -36,5 +64,42 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e){
             Log.v("MyActivity", e.toString());
         }
+    }
+
+    public String readfromFile(){
+
+        String result = "";
+
+        try{
+
+            InputStream inputStream = openFileInput("dairy.txt");
+
+            if( inputStream != null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+             //create a bufferRreader to interpret (read and store data) the inputStream further
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String tempString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+            //assigns value read from bufferRead into tempString
+                while ( (tempString =bufferedReader.readLine()) != null){
+
+                    stringBuilder.append(tempString);
+                }
+
+                inputStream.close();
+
+                result = stringBuilder.toString();
+
+            }
+        } catch(FileNotFoundException e){
+            Log.v("MyActivity", "File Not found " + e.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
